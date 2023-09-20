093c614d468f23d61bfac155fc6196c4d9d8d201
abdellah34 <aammari.1989@gmail.com>
2023-09-20 13:27:22 -0700

    errors correction

--- comand-finder.c
+++ comand-finder.c
@@ -1,4 +1,4 @@
-#include "shel.h"
+#include "shell.h"
 
 
 /**
--- str_cat.c
+++ str_cat.c
@@ -1,7 +1,5 @@
-#include <shell.h>
+#include "shell.h"
 #include <time.h>
-#include "main.h"
-
 /**
 * _strcat -  Entrypoint
 * Description: 'the program's description _strcat
